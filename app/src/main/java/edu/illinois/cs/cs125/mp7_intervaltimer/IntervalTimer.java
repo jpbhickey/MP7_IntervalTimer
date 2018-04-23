41ebe58b828b1a7908ca46a2e9945ec6e71f585b
jpbhickey <jph4@illinois.edu>
2018-04-23 13:40:06 -0500

    Initial commit

---
+++ app/src/main/java/edu/illinois/cs/cs125/mp7_intervaltimer/IntervalTimer.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.mp7_intervaltimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntervalTimer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interval_timer);
+    }
+}
